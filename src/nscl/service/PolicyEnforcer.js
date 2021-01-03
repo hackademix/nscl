23c98dc7c48c714a40ed749c4f94432a3f320226
hackademix <giorgio@maone.net>
2021-01-03 17:23:05 +0100

    PolicyEnforcer stub.

---
+++ src/nscl/service/PolicyEnforcer.js
@@ -0,0 +1,31 @@
+ "use strict";
+/**
+ * This class will use the best strategy available on the current browser platform
+ * (sniffed at object construction time) in order to enforce a given policy 
+ * (see nscl/common/Policy.js) 
+ */
+class PolicyEnforcer {
+  
+  /**
+   * Creates a PolicyEnforcer object, sniffing the available APIS and configuring
+   * the best enforcing strategy.
+   * At the moment, the most likely paths are 2:
+   * 1) blocking webRequest + contentScripts.register() on Firefox
+   * 2) declarativeNetRequest + scripting.registerContentScript() on Chromium
+   * Chromium's new APIs are still a moving target for important use cases, see
+   * - https://bugs.chromium.org/p/chromium/issues/detail?id=1043200
+   * - https://bugs.chromium.org/p/chromium/issues/detail?id=1128112
+   * - https://bugs.chromium.org/p/chromium/issues/detail?id=1054624
+   */
+  costructor() {
+  }
+  
+  /**
+   * Enforces the given policy, or stops enforcing any policy if passed null
+   * 
+   * @param {object} policy the nscl/common/Policy instance to be enforced (or JSON serialization, or null)
+   */
+  async enforce(policy) {
+
+  }
+} 

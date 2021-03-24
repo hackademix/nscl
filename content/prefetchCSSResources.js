11d5a840b4875ad6e9e2ad36116c47d8e75800c3
hackademix <giorgio@maone.net>
2021-03-25 00:44:57 +0100

    More selective and stealthy CSS PP0 protection, to be optionally deployed even on scripting-enabled pages (e.g. in JS-Shield).

--- content/prefetchCSSResources.js
+++ content/prefetchCSSResources.js
@@ -1,14 +1,108 @@
+// depends on /nscl/common/uuid.js
+
 "use strict";
 function prefetchCSSResources(only3rdParty = false, ruleCallback = null) {
-  async function sendMessage(type, args) {
+  async function sendMessage(type, opts) {
     return await browser.runtime.sendMessage({
       __prefetchCSSResources__: {
-        type, args
+        type, opts
       }
     });
   }
-  let corsEnabled = sendMessage("enableCORS");
-  let ghostDoc;
+
+  let createElement = tagName => document.createElementNS("http://www.w3.org/1999/xhtml", tagName);
+
+  let corsSheetURLs = new Set();
+
+  let eventId = uuid();
+  let env = {rulesEvent: `rules:${eventId}`, disabledEvent: `disabled:${eventId}`};
+  window.addEventListener(env.rulesEvent, e => {
+    if (corsSheetURLs.has(e.target.sheet.href)) e.preventDefault();
+  }, true);
+  window.addEventListener(env.disabledEvent, e => {
+    let node = e.target;
+    if (node._keepDisabled || (node.sheet && node.sheet._keepDisabled)) {
+      e.preventDefault();
+    }
+  });
+
+  let wrapCssAccess = (win, env) => {
+    let { StyleSheet } = win;
+    let ssProto = StyleSheet.prototype;
+    let cssProto = win.CSSStyleSheet.prototype;
+    // prevent getting fooled by redefined getters
+    let getOwnerNode = Object.getOwnPropertyDescriptor(ssProto, "ownerNode").get;
+    let fire = (target, event) => {
+      if (target instanceof StyleSheet) target = getOwnerNode.apply(rarget);
+      return target && target.dispatchEvent(new Event(event, {composed: true}));
+    };
+    if (!window.wrappedJSObject) {
+      // Only for Chromium, requiring relaxed CORS and therefore
+      // needing cssRules protection for "privileged" cross-site links.
+      for (let prop of ["rules", "cssRules"]) {
+        let originalGetter = Object.getOwnPropertyDescriptor(cssProto, prop).get;
+        exportFunction(function() {
+          if (fire(this, env.rulesEvent)) {
+            throw new DOMException("Security Error",
+              `Failed to read the '${prop}' property from 'CSSStyleSheet': Cannot access rules`);
+          }
+          return originalGetter.apply(this);
+        }, cssProto, {defineAs: `get ${prop}`});
+      }
+    }
+    let mmProto = win.MediaList.prototype;
+    let { appendMedium, deleteMedium, item } = mmProto;
+    // make disable property temporarily readonly if tagged as _keepDisabled
+    for (let p of [ssProto, win.HTMLStyleElement.prototype, win.HTMLLinkElement.prototype]) {
+      let prop = "media";
+      let des = Object.getOwnPropertyDescriptor(p, prop);
+      exportFunction(function(value) {
+        if (!fire(this, env.disabledEvent)) {
+          if (this instanceof StyleSheet) {
+            return new Proxy(this.media, {
+              get(target, prop, receiver) {
+                if (typeof target[prop] === "function") {
+                  return new Proxy(target[prop], {
+                    apply(target, that, args) {
+                      if (target === appendMedium || target === deleteMedium) {
+                        return;
+                      }
+                      if (target === item) {
+                        return null;
+                      }
+                      return Reflect.apply(...arguments);
+                    }
+                  });
+                }
+                switch(prop) {
+                  case "length":
+                    return 0;
+                  case "mediaText":
+                    return ""
+                }
+                return Reflect.get(...arguments);
+              },
+              set(target, prop, newVal) {
+                switch(prop) {
+                  case "mediaText": return "";
+                }
+                return Reflect.set(...arguments);
+             }
+            });
+          }
+          return "";
+        }
+        return des.get.call(this, value);
+      }, p, {defineAs: `get ${prop}`});
+      exportFunction(function(value) {
+        if (!fire(this, env.disabledEvent)) {
+          return value;
+        }
+        return des.set.call(this, value);
+      }, p, {defineAs: `set ${prop}`});
+    }
+  }
+  patchWindow(wrapCssAccess, env);
 
   if (typeof ruleCallback !== "function") {
     ruleCallback = null;
@@ -18,7 +112,38 @@ function prefetchCSSResources(only3rdParty = false, ruleCallback = null) {
   let { hostname } = location;
   let { baseURI } = document;
   let resources = new Set();
-  let disabled = new WeakSet();
+
+  const MEDIA_DISABLER = "speech and (width > 0px)"
+
+  let keepDisabled = (o, v = true) => {
+    if (v && o._keepDisabled) return false;
+    let isSheet = o instanceof StyleSheet;
+    if (!("_keepDisabled" in o || isSheet)) {
+      if (o instanceof HTMLStyleElement) {
+        observer.observe(o, { characterData: true, attributeFilter: ["media"] });
+      } else {
+        observer.observe(o, {attributeFilter: ["href", "media", "rel"]});
+      }
+    }
+
+    o._keepDisabled = v;
+    let toggleMedia = (o, prop, disabler = MEDIA_DISABLER) => {
+      if (v) {
+        if (!("_originalMedia" in o)) {
+          o._originalMedia = o[prop];
+          o[prop] = disabler;
+        }
+      } else if ("_originalMedia" in o) {
+        o[prop] = o._originalMedia;
+        delete o._originalMedia;
+      }
+    }
+    toggleMedia(...(o instanceof StyleSheet ?
+      [o.media, "mediaText"]
+      : [o, "media"]));
+    return true;
+  };
+
   // we can afford strict parsing because cssText gets normalized
   let resourceFinderRx = /url\("([^"]+)/g;
 
@@ -29,11 +154,15 @@ function prefetchCSSResources(only3rdParty = false, ruleCallback = null) {
           process(rule.styleSheet);
         } else {
           let loader = new Image();
-          loader.onerror = () => process(rule.styleSheet);
-          loader.src = rule.href;
+          return new Promise(resolve => {
+            loader.onerror = loader.onload = () => {
+              resolve(process(rule.styleSheet));
+            }
+            loader.src = rule.href;
+          });
         }
       }
-      return;
+      return false;
     }
     let { cssText, parentStyleSheet } = rule;
     let base = parentStyleSheet.href || baseURI;
@@ -50,68 +179,76 @@ function prefetchCSSResources(only3rdParty = false, ruleCallback = null) {
         continue;
       }
 
-      let { href, origin } = url;
+      let { origin } = url;
       if (resources.has(origin)) continue;
       resources.add(origin);
       if (ruleCallback && ruleCallback(rule, url)) {
         // if ruleCallback returns true we assume it handled or suppressed prefetching by itself
         continue;
       }
-      // Unfortunately it seems we need to actually prefetch the resource due to dns-prefetch unreliablity.
-      new Image().src = href;
+      let { href } = url; // href might have been transformed by the callback
+      // First let's try with DNS prefetching, which works nicely on Chromium.
+      try {
+        let l = createElement("link");
+        l.href = href;
+        l.rel = "dns-prefetch";
+        document.documentElement.insertBefore(l, null);
+        l.remove();
+      } catch (e) {}
+      // Unfortunately sometimes we need to actually prefetch the resource due to dns-prefetch unreliablity on Firefox.
+      new Image().src = url.href;
     }
+    return false;
   };
 
   let process = sheet => {
-    if (processed.has(sheet)) return;
+    if (!sheet || processed.has(sheet)) return;
     processed.add(sheet);
     let { ownerNode } = sheet;
     let rules;
     try {
       rules = sheet.cssRules;
     } catch (e) {
-      if (ownerNode && ownerNode._prefetching) {
+      let {href} = sheet;
+
+      if (!/^(https?):/.test(href) || ownerNode && ownerNode._prefetching === href) {
+        // just give up: either it's another extension (e.g. Stylus), or we've already tried, failing
         console.error("Error processing sheet", sheet, e);
         return;
       }
-      sheet.disabled = true;
+      keepDisabled(sheet);
 
-      // hack needed because disabled doesn't work on CSSImportRule.styleSheet
-      let originalMedia = sheet.media.mediaText;
-      if (sheet.ownerRule) sheet.media.mediaText = "speech and (width > 0)";
-
-      let parent = ownerNode && ownerNode.parentElement || document.documentElement;
-      let link = document.createElementNS("http://www.w3.org/1999/xhtml", "link");
-      link.href = sheet.href;
+      let link = createElement("link");
+      let url = `${href}#${uuid()}`;
+      corsSheetURLs.add(link._prefetching = link.href = url);
       link.rel = "stylesheet";
       link.type = "text/css";
       link.crossOrigin = "anonymous";
-      link._prefetching = true;
-      link.onload = () => {
-        link.sheet.disabled = true;
-        process(link.sheet);
-        link.remove();
-        sheet.media.mediaText = originalMedia;
-        sheet.disabled = false;
-      }
       link.onerror = () => {
         console.error("Error fetching", link);
       }
-      (async () => {
-        await corsEnabled;
-        parent.insertBefore(link, ownerNode || null);
-      })();
-      return;
+      return new Promise(resolve => {
+        link.onload = () => {
+          resolve(process(link.sheet));
+          link.remove();
+          keepDisabled(sheet, false);
+          if (ownerNode) keepDisabled(ownerNode, false);
+        }
+        (async () => {
+          await sendMessage("enableCORS", {url});
+          let parent = ownerNode && ownerNode.parentElement || document.documentElement;
+          parent.insertBefore(link, ownerNode || null);
+        })();
+      });
     }
+    keepDisabled(sheet);
+    let pending = [];
     for (let rule of sheet.cssRules) {
-      checkRule(rule);
+      pending.push(checkRule(rule));
     }
-    if (ownerNode instanceof HTMLStyleElement) {
-      if (ownerNode.disabled && disabled.has(ownerNode)) {
-        ownerNode.disabled = false;
-        disabled.delete(ownerNode);
-      }
-      observer.observe(ownerNode, { characterData: true });
+    Promise.all(pending).then(() => keepDisabled(sheet, false));
+    if (ownerNode) {
+      keepDisabled(ownerNode, false);
     }
   };
 
@@ -121,40 +258,63 @@ function prefetchCSSResources(only3rdParty = false, ruleCallback = null) {
     }
   }
 
-  let checkInlineImport = styleNode => {
-    if (styleNode instanceof HTMLStyleElement && !styleNode.disabled) {
+  let checkNode = styleNode => {
+    if (styleNode._keepDisabled) return;
+    let { sheet } = styleNode;
+    if (sheet) {
+      process(sheet);
+    } else if (styleNode instanceof HTMLStyleElement) {
       let { textContent } = styleNode;
       if (/(?:^|[\s;}])@import\b/i.test(textContent)) {
-        let { sheet } = styleNode;
-        if (sheet) {
-          process(sheet);
-        } else {
-          styleNode.disabled = true;
-          disabled.add(styleNode);
-          let importFinderRx = /(?:^|[\s;}])@import\s*(?:url\(\s*['"]?|['"])([^'"]+)/gi;
-          for (let m; m = importFinderRx.exec(textContent);) {
-            try {
-              let url = new URL(m[1], baseURI);
-              let loader = new Image();
-              loader.onerror = e => {
-                process(styleNode.sheet)
-              };
-              loader.src = url;
-            } catch (e) { }
-          }
+        keepDisabled(styleNode);
+        let importFinderRx = /(?:^|[\s;}])@import\s*(?:url\(\s*['"]?|['"])([^'"]+)/gi;
+        for (let m; m = importFinderRx.exec(textContent);) {
+          try {
+            let url = new URL(m[1], baseURI);
+            let loader = new Image();
+            loader.onerror = e => {
+              process(styleNode.sheet)
+            };
+            loader.src = url;
+          } catch (e) { }
         }
       }
+    } else if (styleNode instanceof HTMLLinkElement
+      && styleNode.relList.contains("stylesheet")
+      && styleNode.href) {
+      keepDisabled(styleNode);
     }
   }
 
   let observer = new MutationObserver(records => {
     for (let r of records) {
+      switch(r.type) {
+        case "childList": // any new link / style element?
+          if (r.addedNodes) {
+            for (let n of r.addedNodes) {
+              checkNode(n);
+            }
+          }
+          break;
+        case "characterData": // content of a style element changed?
+          checkNode(r.target.parentElement);
+        break;
+        case "attributes":
+          if (r.target._keepDisabled) {
+            if (r.attributeName === "media" && r.media !== MEDIA_DISABLER) {
+              r._originalMedia = r.media;
+              r.media = MEDIA_DISABLER;
+            }
+          } else {
+            checkNode(r.target);
+          }
+      }
       if (r.addedNodes) {
         for (let n of r.addedNodes) {
-          checkInlineImport(n);
+          checkNode(n);
         }
       } else if (r.type === "characterData") {
-        checkInlineImport(r.target.parentElement);
+        checkNode(r.target.parentElement);
       }
     }
     processAll();
@@ -170,13 +330,10 @@ function prefetchCSSResources(only3rdParty = false, ruleCallback = null) {
 
   document.addEventListener("readystatechange", () => {
     processAll();
-    if (document.readyState === "complete") {
-      sendMessage("disableCORS");
-    }
   }, true);
 
   for (let styleNode of document.querySelectorAll("style")) {
-    checkInlineImport(styleNode);
+    checkNode(styleNode);
   }
   processAll();
 }
--- service/prefetchCSSResources.js
+++ service/prefetchCSSResources.js
@@ -1,58 +1,128 @@
 "use strict";
 {
-  let enabled = new Map();
+  let requiredCORS = !UA.isMozilla;
 
+  let enabled = new Map();
+  let corsInfoCache = new Map();
   browser.tabs.onRemoved.addListener(tab => {
     enabled.delete(tab.id);
   });
+  let reqKey = (frameId, destination, origin) => `${frameId}|${destination}@${origin}`;
 
   browser.runtime.onMessage.addListener(
-    ({__prefetchCSSResources__: msg }, sender) => {
+    ({__prefetchCSSResources__: msg}, sender) => {
       if (!msg) return;
       let {tab, url, origin} = sender;
       if (!origin) origin = new URL(url).origin;
-      let origins = enabled.get(tab.id);
+      let requests = enabled.get(tab.id);
       switch(msg.type) {
         case "enableCORS":
-         if (!origins) {
-            enabled.set(tab.id, origins = new Set());
+         if (!requests) {
+            enabled.set(tab.id, requests = new Set());
           }
-          origins.add(origin);
-        return Promise.resolve(true);
-        case "disableCORS":
-          if (origins) {
-            origins.delete(origin);
-            return Promise.resolve(true);
-          }
-        break;
+          requests.add(reqKey(sender.frameId, msg.opts.url, origin));
+          return Promise.resolve(true);
       }
       return Promise.resolve(false);
   });
 
-  browser.webRequest.onHeadersReceived.addListener(r => {
-    let {tabId} = r;
-    if (!enabled.has(tabId)) return;
-    let origin = new URL(r.initiator || r.originUrl).origin;
-    if (!enabled.get(tabId).has(origin)) return;
+  let corsInfo = (r, forget = false) => {
+    let {tabId, frameId, url, requestId} = r;
+    if (corsInfoCache.has(requestId)) {
+      let cached = corsInfoCache.get(requestId);
+      if (forget) corsInfoCache.delete(requestId);
+      return cached;
+    }
+    let origin = new URL(r.initiator || r.originUrl || r.documentUrl).origin;
+    let destination = new URL(url).origin;
+    let info;
+    if (destination !== origin) {
+      info = {origin};
+      let requests = enabled.get(tabId);
+      if (requests) {
+        let key = reqKey(frameId, url, origin);
+        info.authorize = requests.has(key);
+        requests.delete(key);
+      }
+    } else {
+      info = null;
+    }
+    corsInfoCache.set(requestId, info);
+    return info;
+  }
 
+
+  let allCssFilter =  {
+    urls: ['<all_urls>'],
+    types: ['stylesheet']
+  };
+
+  let options = ['blocking'];
+  browser.webRequest.onBeforeRequest.addListener(r => {
+    corsInfo(r); // needed to cache corseInfo keying by requestId, instead of URL w/ #hash
+  }, allCssFilter, options);
+
+  options.push('requestHeaders');
+  browser.webRequest.onBeforeSendHeaders.addListener(r => {
+    let crossSite = corsInfo(r);
+    if (!(crossSite && crossSite.authorize)) return;
+    // her we try to force a cached response
+    let {requestHeaders} = r;
+    for (let h of requestHeaders) {
+      let name = h.name.toLowerCase();
+      if (name === "cache-control") {
+        h.value = "max-age=604800"
+      }
+    }
+    return {requestHeaders};
+  }, allCssFilter, options);
+
+  options[1] = 'responseHeaders';
+  if (requiredCORS) {
+    options.push('extraHeaders'); // required by Chromium to handle CORS headers
+  }
+
+  browser.webRequest.onHeadersReceived.addListener(r => {
+    let crossSite = corsInfo(r);
+    if (!crossSite) return;
+    let {authorize, origin} = crossSite;
     let {responseHeaders} = r;
-    let found = false;
+
+    if (authorize && !requiredCORS) return; // on Firefox we just need caching
+
+    let headersPatch = Object.assign(Object.create(null), authorize
+      ? {
+        "cache-control": "no-store",
+        "vary": "origin",
+        "access-control-allow-origin": origin
+      }
+      : {
+        "cache-control": "private, max-age=604800, immutable"
+      });
+
     for (let h of responseHeaders) {
-      if (h.name.toLowerCase() === "access-control-allow-origin") {
-        h.value = "*";
-        found = true;
+      let name = h.name.toLowerCase();
+      if (name in headersPatch) {
+        h.value = headersPatch[name];
+        delete headersPatch[name];
       }
     }
 
-    if (!found) {
-      responseHeaders.push({
-        'name': 'Access-Control-Allow-Origin',
-        'value': '*'
-      });
+    for (let [name, value] of Object.entries(headersPatch)) {
+      responseHeaders.push({name, value});
     }
+
     return {responseHeaders};
-  }, {
-    urls: ['<all_urls>'],
-    types: ['stylesheet']
-  }, ['blocking', 'responseHeaders', 'extraHeaders']);
+  }, allCssFilter, options);
+
+  let cleanup = r => {
+    let crossSite = corsInfo(r, true);
+    if (!(crossSite && crossSite.authorize)) return;
+    if (!r.fromCache) {
+      debug("Warning: cross-site CSS request from CSS resource prefetching NOT from cache.");
+    }
+  }
+  for (let ev of ["onCompleted", "onErrorOccurred"]) {
+    browser.webRequest[ev].addListener(cleanup, allCssFilter);
+  }
 }

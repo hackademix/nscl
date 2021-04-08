aaabb903b8cb09cc9197f03fbfba95b7280e41e6
hackademix <giorgio@maone.net>
2021-04-08 12:03:41 +0200

    Encapsulated event-based communication between unprivileged patch and priviliged content script into a Port object.

--- content/patchWindow.js
+++ content/patchWindow.js
@@ -1,6 +1,44 @@
 "use strict";
+/**
+ * Injects code into page context in a cross-browser way, providing it
+ * with tools to wrap/patch the DOM and the JavaScript environment
+ * and propagating the changes to child windows created on the fly in order
+ * to prevent the modifications to be cancelled by hostile code.
+ *
+ * @param {function} patchingCallback
+ *        the (semi)privileged wrapping code to be injected.
+ *        Warning: this is not to be considered a closure, since Chromium
+ *        injection needs it to be reparsed out of context.
+ *        Use the env argument to propagate parameters.
+ *        It will be called as patchingCallback(unwrappedWindow, env).
+ * @param {object} env
+ *        a JSON-serializable object to made available to patchingCallback as
+ *        its second argument*
+ * @returns {object} port
+ *        A Port object to be used to communicate with the privileged content
+ *        script, by using port.postMessage(msg, [event]) and
+ *        the port.onMessage(msg, event) user-defined callback.
+ */
 
 function patchWindow(patchingCallback, env = {}) {
+  let eventId = this && this.eventId || `windowPatchMessages:${uuid()}`;
+  let { dispatchEvent, addEventListener } = window;
+
+  function Port(from, to) {
+    this.postMessage = function(msg, target = window) {
+      let detail = {msg};
+      dispatchEvent.call(target, new CustomEvent(`${eventId}:${to}`, {detail, composed: true}));
+      return detail.ret;
+    };
+    addEventListener.call(window, `${eventId}:${from}`, e => {
+      if (typeof this.onMessage === "function" && e.detail) {
+        e.detail.ret = this.onMessage(e.detail.msg, e);
+      }
+    }, true);
+    this.onMessage = null;
+  }
+  let port = new Port("extension", "page");
+
   let nativeExport = this && this.exportFunction || typeof exportFunction == "function";
   if (!nativeExport) {
     // Chromium
@@ -44,18 +82,22 @@ function patchWindow(patchingCallback, env = {}) {
       let patchWindow = ${patchWindow};
       let cloneInto = ${cloneInto};
       let exportFunction = ${exportFunction};
+      let env = ${JSON.stringify(env)};
+      let eventId = ${JSON.stringify(eventId)};
+      env.port = new (${Port})("page", "extension");
       ({
         patchWindow,
         exportFunction,
         cloneInto,
-      }).patchWindow(${patchingCallback}, ${JSON.stringify(env)});
+        eventId,
+      }).patchWindow(${patchingCallback}, env);
     })();
     `;
     document.documentElement.insertBefore(script, document.documentElement.firstChild);
     script.remove();
-    return;
+    return port;
   }
-
+  env.port = new Port("page", "extension");
   // win: window object to modify.
   // modifyTarget: callback to function that modifies the desired properties
   //                or methods. Callback must take target window as argument.
@@ -113,5 +155,6 @@ function patchWindow(patchingCallback, env = {}) {
     Object.defineProperty(wrappedProto, property, descriptor);
   }
 
-  return modifyWindow(window, patchingCallback);
+  modifyWindow(window, patchingCallback);
+  return port;
 }

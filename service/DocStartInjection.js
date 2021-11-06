75dd2317e1c74c765afabb33375462b199498b2a
Aaron Kollasch <aaron@kollasch.dev>
2021-11-06 16:16:56 -0400

    Add support for container tabs

--- service/DocStartInjection.js
+++ service/DocStartInjection.js
@@ -46,7 +46,7 @@ var DocStartInjection = (() => {
 
   async function begin(request) {
     let scripts = new Set();
-    let {tabId, frameId, url, type, documentId, documentLifecycle, frameType} = request;
+    let {tabId, frameId, cookieStoreId, url, type, documentId, documentLifecycle, frameType} = request;
 
     if (tabId < 0 || !/^(?:(?:https?|ftp|data|blob|file):|about:blank$)/.test(url)) return;
 
@@ -59,7 +59,7 @@ var DocStartInjection = (() => {
     await Promise.allSettled([...scriptBuilders].map(async buildScript => {
       let script;
       try {
-        script = await buildScript({tabId, frameId, url, type});
+        script = await buildScript({tabId, frameId, cookieStoreId, url, type});
         if (!script) return;
         if (mv3Callbacks) {
           if (typeof script !== "object") {

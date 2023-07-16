fd083f7200cf9357fc2e907b9609fe8103982c9a
hackademix <giorgio@maone.net>
2023-07-16 17:55:24 +0200

    DebuggableRegExp class.

---
+++ common/DebuggableRegExp.js
@@ -0,0 +1,54 @@
+/*
+ * NoScript Commons Library
+ * Reusable building blocks for cross-browser security/privacy WebExtensions.
+ * Copyright (C) 2020-2023 Giorgio Maone <https://maone.net>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+"use strict";
+
+var DebuggableRegExp = (() => {
+
+  return class DebuggableRegExp {
+
+    constructor(rx) {
+      this.originalRx = rx;
+      const chunks = rx.source.split("|");
+      this._parts = [];
+      let curPart = [];
+      for (const c of chunks) {
+        curPart.push(c);
+        try {
+          this._parts.push(new RegExp(curPart.join("|")));
+          curPart = [];
+        } catch (e) {
+        }
+      }
+    }
+
+    test(s) {
+      for (let part of this._parts) {
+        try {
+          if (part.test(s)) return true;
+        } catch (e) {
+          throw new Error(`${e.message}\ntesting RegExp:\n${part}\non string:\n${s}`);
+        }
+      }
+      return false;
+    }
+  };
+
+})();

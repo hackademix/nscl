b47f167eb62c032908cce51177c03fa5429c1f45
hackademix <giorgio@maone.net>
2024-07-29 09:51:25 +0200

    Use native crypto UUID generator when available.

--- common/uuid.js
+++ common/uuid.js
@@ -19,17 +19,27 @@
  */
 
 'use strict';
+{
+  const _impl = "randomUUID" in crypto
+    ? () => crypto.randomUUID()
+    : () =>
+      ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
+          c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4)
+          .toString(16))
+    ;
 
-function uuid() {
-  try {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
-      c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4)
-            .toString(16));
-  } catch (e) {
-    // fallback, as the Tor Browser seems to fail above
-    uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  // if crypto API is missing or broken
+  const _fallback =  () =>
+    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
       let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
+
+  function uuid() {
+    try {
+      return _impl();
+    } catch (e) {
+      return _fallback();
+    }
   }
 }

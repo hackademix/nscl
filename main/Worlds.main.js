3b895d70938ff3bd8a1f4de551499905c06e4b05
hackademix <giorgio@maone.net>
2024-11-13 00:21:19 +0100

    Simplified and improved Worlds.js handshaking.

--- main/Worlds.main.js
+++ main/Worlds.main.js
@@ -20,78 +20,85 @@
 
 "use strict";
 
-Worlds?.exportFunction ||= (func, targetObject, {defineAs, original} = {}) => {
-  try {
-    let  [propDef, getOrSet, propName] = defineAs && /^([gs]et)(?:\s+(\w+))$/.exec(defineAs) || [null, null, defineAs];
-    let propDes = propName && Object.getOwnPropertyDescriptor(targetObject, propName);
-    if (getOrSet && !propDes) { // escalate through prototype chain
-      for (let proto = Object.getPrototypeOf(targetObject); proto; proto = Object.getPrototypeOf(proto)) {
-        propDes = Object.getOwnPropertyDescriptor(proto, propName);
-        if (propDes) {
-          targetObject = proto;
-          break;
-        }
-      }
-    }
+if (globalThis.Worlds?.main) {
+  const {Function, Object, Proxy, Promise, Reflect, WeakMap} = globalThis;
+  const {console} = Worlds.main;
 
-    let toString = Function.prototype.toString;
-    let strVal;
-    if (!original) {
-      original = propDef && propDes ? propDes[getOrSet] : defineAs && targetObject[defineAs];
-    }
-    if (!original) {
-      // It seems to be a brand new function, rather than a replacement.
-      // Let's ensure it appears as a native one with little hack: we proxy a Promise callback ;)
-      Promise.resolve(new Promise(resolve => original = resolve));
-      let name = propDef && propDes ? `${getOrSet} ${propName}` : defineAs;
-      if (name) {
-        let nameDef = Reflect.getOwnPropertyDescriptor(original, "name");
-        nameDef.value = name;
-        Reflect.defineProperty(original, "name", nameDef);
-        strVal = toString.call(original).replace(/^function \(\)/, `function ${name}()`)
-      }
-    }
-
-    strVal = strVal || toString.call(original);
-
-    let proxy = new Proxy(original, {
-      apply(target, thisArg, args) {
-        return func.apply(thisArg, args);
-      }
-    });
-
-    if (!exportFunction._toStringMap) {
-      let map = new WeakMap();
-      exportFunction._toStringMap = map;
-      let toStringProxy = new Proxy(toString, {
-        apply(target, thisArg, args) {
-          return map.has(thisArg) ? map.get(thisArg) : Reflect.apply(target, thisArg, args);
-        }
-      });
-      map.set(toStringProxy, toString.apply(toString));
-      Function.prototype.toString = toStringProxy;
-    }
-    exportFunction._toStringMap.set(proxy, strVal);
-
-    if (propName) {
-      if (!propDes) {
-        targetObject[propName] = proxy;
-      } else {
-        if (getOrSet) {
-          propDes[getOrSet] = proxy;
-        } else {
-          if ("value" in propDes) {
-            propDes.value = proxy;
-          } else {
-            return exportFunction(() => proxy, targetObject, `get ${propName}`);
+  Object.freeze(Object.assign(Worlds.main, {
+    exportFunction(func, targetObject, {defineAs, original} = {}) {
+      try {
+        let  [propDef, getOrSet, propName] = defineAs && /^([gs]et)(?:\s+(\w+))$/.exec(defineAs) || [null, null, defineAs];
+        let propDes = propName && Object.getOwnPropertyDescriptor(targetObject, propName);
+        if (getOrSet && !propDes) { // escalate through prototype chain
+          for (let proto = Object.getPrototypeOf(targetObject); proto; proto = Object.getPrototypeOf(proto)) {
+            propDes = Object.getOwnPropertyDescriptor(proto, propName);
+            if (propDes) {
+              targetObject = proto;
+              break;
+            }
           }
         }
-        Object.defineProperty(targetObject, propName, propDes);
+
+        let toString = Function.prototype.toString;
+        let strVal;
+        if (!original) {
+          original = propDef && propDes ? propDes[getOrSet] : defineAs && targetObject[defineAs];
+        }
+        if (!original) {
+          // It seems to be a brand new function, rather than a replacement.
+          // Let's ensure it appears as a native one with little hack: we proxy a Promise callback ;)
+          Promise.resolve(new Promise(resolve => original = resolve));
+          let name = propDef && propDes ? `${getOrSet} ${propName}` : defineAs;
+          if (name) {
+            let nameDef = Reflect.getOwnPropertyDescriptor(original, "name");
+            nameDef.value = name;
+            Reflect.defineProperty(original, "name", nameDef);
+            strVal = toString.call(original).replace(/^function \(\)/, `function ${name}()`)
+          }
+        }
+
+        strVal = strVal || toString.call(original);
+
+        let proxy = new Proxy(original, {
+          apply(target, thisArg, args) {
+            return func.apply(thisArg, args);
+          }
+        });
+
+        if (!exportFunction._toStringMap) {
+          let map = new WeakMap();
+          exportFunction._toStringMap = map;
+          let toStringProxy = new Proxy(toString, {
+            apply(target, thisArg, args) {
+              return map.has(thisArg) ? map.get(thisArg) : Reflect.apply(target, thisArg, args);
+            }
+          });
+          map.set(toStringProxy, toString.apply(toString));
+          Function.prototype.toString = toStringProxy;
+        }
+        exportFunction._toStringMap.set(proxy, strVal);
+
+        if (propName) {
+          if (!propDes) {
+            targetObject[propName] = proxy;
+          } else {
+            if (getOrSet) {
+              propDes[getOrSet] = proxy;
+            } else {
+              if ("value" in propDes) {
+                propDes.value = proxy;
+              } else {
+                return exportFunction(() => proxy, targetObject, `get ${propName}`);
+              }
+            }
+            Object.defineProperty(targetObject, propName, propDes);
+          }
+        }
+        return proxy;
+      } catch (e) {
+        console.error(e, `setting ${targetObject}.${defineAs || original}`, func);
       }
-    }
-    return proxy;
-  } catch (e) {
-    console.error(e, `setting ${targetObject}.${defineAs || original}`, func);
-  }
-  return null;
-};
+      return null;
+    },
+  }));
+}

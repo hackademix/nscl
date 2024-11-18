b9610d41876354963584737b3ce668e6583e0546
hackademix <giorgio@maone.net>
2024-11-18 09:53:18 +0100

    Fix inclusion.sh to account for extra dots in file names (e.g. Worlds.main.js).

--- include.sh
+++ include.sh
@@ -25,11 +25,15 @@ fi
 [[ -d "$TARGET/nscl" ]] && rm -rf "$TARGET"/nscl/**
 
 filter_inclusions() {
+  if [[ $1 = "-q" ]]; then
+     QUIET=1
+     shift
+  fi
   echo "Processing inclusions referenced by $@..."
   pushd >/dev/null 2>&1 "$1"
   shift
   shopt -s globstar nullglob
-  for f in $(grep -Eho '\bnscl/[0-9a-zA-Z_/-]+\.js' **/*.{js,html} "$@" | sort | uniq); do
+  for f in $(grep -Eho '\bnscl/[0-9a-zA-Z_/.-]+\.js' **/*.{js,html} "$@" | sort | uniq); do
     if ! [[ -f "$TARGET/$f" ]]; then
       nscl_srcdir="$(dirname "$f")"
       # create symlink to actual file if this is a MAIN world alias
@@ -47,10 +51,10 @@ filter_inclusions() {
       fi
       nscl_curdir="$TARGET/$nscl_srcdir"
       mkdir -p "$nscl_curdir"
-      cp -p "$SRC/$f" "$nscl_curdir"
+      cp -pP "$SRC/$f" "$nscl_curdir"
       echo "Including $f in $nscl_curdir"
     else
-      echo >&2 "$TARGET/$f exists!"
+      [[ $QUIET ]] || echo >&2 "$TARGET/$f exists!"
     fi
   done
   popd >/dev/null 2>&1
@@ -59,7 +63,7 @@ filter_inclusions() {
 SERVICE_WORKER=$(grep -e '"service_worker":' "$TARGET/manifest.json" | sed -re 's/.*: "(.*)",?/\1/')
 filter_inclusions "$TARGET" manifest.json $SERVICE_WORKER
 # include also references from nscl scripts already included
-[[ -d "$TARGET/nscl" ]] && filter_inclusions "$TARGET/nscl/"
+[[ -d "$TARGET/nscl" ]] && filter_inclusions -q "$TARGET/nscl/"
 
 # auto-update TLDs if included
 "$BASE/update-tlds.sh" "$TARGET/nscl/common/tld.js"

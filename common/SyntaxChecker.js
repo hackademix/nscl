f5e47cf682e3d32db5da35abab7f20c1402c5c87
hackademix <giorgio@maone.net>
2026-03-15 17:35:50 +0100

    Remove MV3-condition on acorn.js dependency to account for some browsers (IronFox?) breaking native syntax checking (noscript #535).

--- common/SyntaxChecker.js
+++ common/SyntaxChecker.js
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
-// depends on /nscl/lib/acorn.js MV3
+// depends on /nscl/lib/acorn.js
 class SyntaxChecker {
   constructor() {
     this.lastError = null;
@@ -30,7 +30,7 @@ class SyntaxChecker {
           eval("''");
           this[asyncName] = this[methodName];
         } catch (e) {
-          await include("/nscl/lib/acorn.js"); // MV3
+          await include("/nscl/lib/acorn.js");
           const acornImpl = {
             checkAsync: function (script) {
               const func = `() => {${script}}`;
